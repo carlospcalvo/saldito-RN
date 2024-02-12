72ebe3c780ac978caa3695ab29044ea0bc27af90
Carlos Calvo Nazabal <carloscalvonazabal@gmail.com>
2024-02-12 17:14:41 -0300

    improve new expense date input

--- components/ExpenseForm/DateInput.tsx
+++ components/ExpenseForm/DateInput.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { Pressable, StyleSheet, Text, View } from "react-native";
+import Icon from "@expo/vector-icons/FontAwesome6";
 import { format, parseISO } from "date-fns";
 import DateTimePickerModal from "react-native-modal-datetime-picker";
 import useExpenseStore from "@lib/expense-store";
@@ -23,7 +24,6 @@ export default function DateInput() {
 	return (
 		<>
 			<View style={styles.container}>
-				<Text style={styles.label}>Fecha:</Text>
 				<Pressable
 					style={({ pressed }) => [
 						styles.button,
@@ -31,6 +31,7 @@ export default function DateInput() {
 					]}
 					onPress={toggleDatePicker}
 				>
+					<Icon name="calendar-days" style={styles.icon} />
 					<Text style={styles.currentValueText}>
 						{format(parseISO(date), "dd/MM/yyyy")}
 					</Text>
@@ -58,13 +59,30 @@ const styles = StyleSheet.create({
 		fontFamily: "Raleway_500Medium",
 	},
 	button: {
+		flexDirection: "row",
+		alignItems: "center",
+		gap: 6,
 		borderRadius: 8,
-		backgroundColor: "#dcdee0",
+		// backgroundColor: "#dcdee0",
+		backgroundColor: "white",
 		paddingVertical: 4,
 		paddingHorizontal: 8,
+
+		shadowColor: "#000",
+		shadowOffset: {
+			width: 0,
+			height: 1,
+		},
+		shadowOpacity: 0.2,
+		shadowRadius: 1.41,
+
+		elevation: 2,
 	},
 	buttonPressed: {
 		opacity: 0.5,
+	},
+	icon: {
+		color: "#a955f7",
 	},
 	currentValueText: {
 		fontFamily: "Raleway_400Regular",
